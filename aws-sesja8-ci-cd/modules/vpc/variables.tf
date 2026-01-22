049c322c5f5d8a6fe186f5a1b69c25f0fdda5122
snajper <p.karnas@ligertrading.com>
2026-01-23 00:10:45 +0100

    Add Terraform GitHub Actions CI/CD

---
+++ aws-sesja8-ci-cd/modules/vpc/variables.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "az" {
+  type = string
+}
+
+variable "igw_name" {
+  type = string
+}
+
+variable "rt_name" {
+  type = string
+}
+
